b6fbfeb54eed39e22976a6e4cdeb9d3ff57089cb
JorgeCajape <jorge18marquez@gmail.com>
2020-07-20 20:00:08 -0500

    Intento de API2
    segundo intento al aplicar  una API, resulto pero con fallo, debido a que el codigo es sacadode internet.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-
-    function initMap() {
-      map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8
-      });
-    }
- 
+function iniciarMap(){
+  var coord = {lat:-2.2191452 ,lng: -79.9467276};
+  var map = new google.maps.Map(document.getElementById('map'),{
+    zoom: 10,
+    center: coord
+  });
+  var marker = new google.maps.Marker({
+    position: coord,
+    map: map
+  });
+}
